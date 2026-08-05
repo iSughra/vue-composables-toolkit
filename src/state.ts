b357397279903abb7087bc8b4b355730e24e75aa
iSughra <ysrdora@googlemail.com>
2026-08-06 00:15:04 +0200

    feat(state): add useSessionStorage sync
    Co-authored-by: Sébastien Chopin <904724+Atinux@users.noreply.github.com>

--- src/state.ts
+++ src/state.ts
@@ -4,3 +4,8 @@ export function useLocalStorage(key: string, initialValue: string) {
   watch(data, (newVal) => localStorage.setItem(key, newVal));
   return data;
 }
+export function useSessionStorage(key: string, initialValue: string) {
+  const data = ref(sessionStorage.getItem(key) ?? initialValue);
+  watch(data, (newVal) => sessionStorage.setItem(key, newVal));
+  return data;
+}
