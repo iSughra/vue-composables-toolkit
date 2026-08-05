94e1d120dd448e336afbf68b1b71a1ff0babc8a3
iSughra <ysrdora@googlemail.com>
2026-08-06 00:15:02 +0200

    feat(state): add useLocalStorage with deep reactivity
    Co-authored-by: Daniel Roe <28706372+danielroe@users.noreply.github.com>

---
+++ src/state.ts
@@ -0,0 +1,6 @@
+import { ref, watch } from 'vue';
+export function useLocalStorage(key: string, initialValue: string) {
+  const data = ref(localStorage.getItem(key) ?? initialValue);
+  watch(data, (newVal) => localStorage.setItem(key, newVal));
+  return data;
+}
