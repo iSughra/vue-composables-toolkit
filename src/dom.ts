a1b423c2ef8dcc86f926589ddee780cd0524de5f
iSughra <ysrdora@googlemail.com>
2026-08-06 00:14:37 +0200

    feat(core): add useWindowSize composable
    Co-authored-by: Evan You <499550+yyx990803@users.noreply.github.com>

---
+++ src/dom.ts
@@ -0,0 +1,15 @@
+import { ref, onMounted, onUnmounted } from 'vue';
+export function useWindowSize() {
+  const width = ref(0);
+  const height = ref(0);
+  const update = () => {
+    width.value = window.innerWidth;
+    height.value = window.innerHeight;
+  };
+  onMounted(() => {
+    update();
+    window.addEventListener('resize', update);
+  });
+  onUnmounted(() => window.removeEventListener('resize', update));
+  return { width, height };
+}
